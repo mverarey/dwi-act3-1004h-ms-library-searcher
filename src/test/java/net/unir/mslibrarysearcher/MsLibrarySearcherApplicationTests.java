69d6c70ca9e3f4e894de62ff1ba6248087afe198
onxJi <jaaziel-jz77@outlook.com>
2025-03-01 22:34:55 -0700

    comment test

--- src/test/java/net/unir/mslibrarysearcher/MsLibrarySearcherApplicationTests.java
+++ src/test/java/net/unir/mslibrarysearcher/MsLibrarySearcherApplicationTests.java
@@ -1,4 +1,4 @@
-// package net.unir.mslibrarysearcher;
+/* // package net.unir.mslibrarysearcher;
 
 // import org.junit.jupiter.api.Test;
 // import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@
 // 	void contextLoads() {
 // 	}
 
-// }
+// } */
